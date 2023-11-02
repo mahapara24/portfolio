b588a81c0b466abb3ada18c58420d3ee5a77b294
Mahapara <mahanizamani12345678@gmail.com>
2023-11-02 18:05:47 +0500

    changes to header typewriterchangesdone

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -49,27 +49,56 @@ function Home() {
   });
 
   return (
-    <div className=' h-screen  w-full bg-primary-gray   sm:items-start pt-64 pl-4 sm:pl-40 sm:pt-72'>
-      <div className=' text-left sm:px-6 sm:py-16'>
-        <div className='text-4xl ml-4 items-left text-justify text-white font- font-bold leading-tight sm:text-6xl 
-        '>
-          <h1 style={{marginBottom: '20px'}}>Hello, I'm a {""}
-            <span className=' pb-8 mb-8 p-4 ' style={{  paddingTop:'20rem', fontWeight: 'bold', color: '#FF4A57', fontSize: '4rem' }}>
-            <br className='sm:hidden' />{text}</span>
-            <span style={{ color: '#FF4A57', marginTop: '60px' }}>{" "}
-              <Cursor className='bg-transparent pt-8' cursorStyle='<' />
-            </span>
-          </h1>
+<div className='h-screen w-full bg-primary-gray sm:items-start pt-64 pl-4 sm:pl-40 sm:pt-72'>
+  <div className='text-left sm:px-6 sm:py-16'>
+    <div className='text-2xl ml-4 items-left text-justify text-white font- font-bold leading-tight sm:text-6xl'>
+         <h1 className='hidden sm:block' style={{ marginBottom: '20px' }}>
+        Hello, I'm a {""}
+        <span className='pb-8 mb-8 p-4' style={{ paddingTop: '20rem', fontWeight: 'bold', color: '#FF4A57', fontSize: '4rem' }}>
+          <br className='sm:hidden' />{text}
+        </span>
+        <span style={{ color: '#FF4A57', marginTop: '60px' }}>
+          {" "}
+          <Cursor className='bg-transparent pt-8' cursorStyle='<' />
+        </span>
+    </h1>
+          {/* <div className='text-xl'>
+          <h1 className='sm:hidden md: hidden' style={{ marginBottom: '20px' }}>
+  Hello, I'm a {""}
+  <div className='pb-8 mb-8 p-4 sm:p-8 text-sm ' style={{ paddingTop: '20rem', fontWeight: 'bold', color: '#FF4A57', fontSize: '0.2rem', sm: 'text-xl' }}>
+    <br className='sm:hidden' /><p>{text}</p>
+  </div>
+  <span style={{ color: '#FF4A57', marginTop: '60px' }}>
+    {" "}
+    <Cursor className='bg-transparent pt-8' cursorStyle='<' />  
+  </span>
+</h1>
+</div> */}
 
-          
-           <div className=' flex justify-between gap-8'>
-            <button className='px-2 text-lg mt-4 flex items-start font-normal sm:mt-8 sm:text-xl sm:font-semibold text-white bg-custom-red sm:px-6 py-3 sm:my-3 sm:flex sm:items-center rounded-md border-2 border-custom-red'>
-              <Link to='not/public/MAHAPARA NIZAMANI.pdf' target='_blank'>Download Resume</Link>
-              <span className='ml-2'>
-                <BsFillPersonLinesFill size={25} />
-              </span>
-            </button>
-          </div>
+<div className='text-3xl'>
+  <h1 className='sm:hidden md:hidden' style={{ marginBottom: '20px' }}>
+    Hello, I'm a {""}
+    <div className='sm:hidden md:block'>
+      <p className='text-3xl text-custom-red inline'>{text}</p>
+
+    <span style={{ color: '#FF4A57',  }}>
+      {" "}
+      <Cursor className='bg-primary-gray' cursorStyle='<' />
+    </span>
+    </div>
+  </h1>
+</div>
+
+
+<div className='flex justify-between gap-8'>
+  <button className='px-2 py-2 text-base mt-4 flex items-start font-normal sm:mt-8 sm:text-xl sm:font-semibold text-white bg-custom-red sm:px-6 sm:py-3 sm:my-3 sm:flex sm:items-center rounded-md border-2 border-custom-red'>
+    <Link to='not/public/MAHAPARA NIZAMANI.pdf' target='_blank'>Download Resume</Link>
+    <span className='ml-2'>
+      <BsFillPersonLinesFill size={25} />
+    </span>
+  </button>
+</div>
+
         </div>
       </div>
     </div>

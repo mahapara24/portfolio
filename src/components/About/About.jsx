dd0492ce3a922b67961dd1c2b0d50cfd0c34b0ed
Mahapara Nizamani <mahapara2424@gmail.com>
2024-07-31 11:49:44 +0500

    about updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
 
 const About = () => {
+  useEffect(() => {
+    // Animation for the heading
+    gsap.fromTo(
+      ".about-heading",
+      { opacity: 0, y: -50 },
+      { opacity: 1, y: 0, duration: 1 }
+    );
+
+    // Animation for the paragraph
+    gsap.fromTo(
+      ".about-paragraph",
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 1, delay: 0.5 }
+    );
+  }, []);
+
   return (
     <div
       id="about"
-      className="h-screen  scrollbar-hide scroll-smooth w-full overflow-y-auto pt-20 sm:pt-20 pt pb-5 px-2 bg-primary-gray"
+      className="h-screen scrollbar-hide scroll-smooth w-full overflow-y-auto pt-20 sm:pt-20 pt pb-5 px-2 bg-primary-gray"
     >
-      <div className="max-w-screen-lg mx-auto flex  flex-col justify-center py-5 px-5 text-center sm:text-left">
+      <div className="max-w-screen-lg mx-auto flex flex-col justify-center py-5 px-5 text-center sm:text-left">
         <div>
-          <h2 className="text-2xl sm:text-4xl text-custom-red font-bold ">
+          <h2 className="about-heading text-2xl sm:text-4xl text-custom-red font-bold">
             About
           </h2>
         </div>
-        <p className="text-[17px]  rounded-lg  mt-4 font-normal py-2 sm:text-[20px] sm:font-base sm:tracking-widest text-white">
+        <p className="about-paragraph text-[17px] rounded-lg mt-4 font-normal py-2 sm:text-[20px] sm:font-base sm:tracking-widest text-white">
           I'm Mahapara, I possess a strong foundation in JavaScript, ReactJS,
-          Node.js, ExpressJs, MongoDB and the Tailwind CSS framework. My
+          Node.js, ExpressJs, MongoDB, and the Tailwind CSS framework. My
           expertise lies in building web applications that are both functional
           and visually appealing with proper functionality. I have successfully
           contributed to various projects, demonstrating my proficiency in
